ec653997c303b7237245696de52aea135391576c
Swemy <swemy.lopezhrnd@uanl.edu.mx>
2023-02-08 11:56:09 -0600

    fixed sidebar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-document.querySelector('.toggle-btn');
+// sidebar toggle
+const btnToggle = document.querySelector('.toggle-btn');
 
-btnToggle.addEventListener('click', function(){
-document.getElementById('sidebar').classList.toggle('active');
-
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
 });
